4bff9ad6903c8225dd78789c5893f8d87f1e9c31
Yoshishiro Misawa <misawa@sackle.co.jp>
2013-12-26 16:24:39 +0900

    brunchでビルドしたブートイメージの場合でも起動するように修正 TWRPオプションの無効化

--- device_hw01e.mk
+++ device_hw01e.mk
@@ -7,16 +7,6 @@ $(call inherit-product-if-exists, vendor/huawei/hw01e/hw01e-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/huawei/hw01e/overlay
 
-LOCAL_PATH := device/huawei/hw01e
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
